3c18854a768265ebff387c1e62465fc48cb04c9b
Basma Tarek <basmaelshaer2@gmail.com>
2024-12-22 22:52:18 +0200

    Update and rename oop11-polymorphsim.py to polymorphsim.py

---
+++ polymorphsim.py
@@ -0,0 +1,33 @@
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Drive!")
+
+class Boat:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Sail!")
+
+class Plane:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Porch", "mpdel")     
+boat1 = Boat("Ibiza", "Touring 20") 
+plane1 = Plane("Boeing", "747")    
+
+vehicles = [car1, boat1, plane1] 
+
+for vehicle in vehicles:
+    vehicle.move()  
+
